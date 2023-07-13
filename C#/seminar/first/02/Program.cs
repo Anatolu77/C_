9aaaed11bb3ad11a5478341b0d65ffb990964cf6
anatoly <czuczkov1997@mail.ru>
2023-07-13 23:52:04 +0300

    add new task

--- C#/seminar/first/02/Program.cs
+++ C#/seminar/first/02/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if (max < numberB) max = numberB;
+if (max < numberC) max = numberC;
+Console.WriteLine("Максимальное число из этих чисел = " + max );
