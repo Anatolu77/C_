0cbcb70791b3a1846e1b339b3bd38187983f62a4
anatoly <czuczkov1997@mail.ru>
2023-07-16 22:33:58 +0300

    add new task

---
+++ C#/seminar/third/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+string CoordinateRange(int quarter)
+{
+
+    if (quarter == 1)
+
+}
+
+int GetInfo( string message)
+{
+    Console.Write(message);
+    int numberQuarter = Convert.ToInt32(Console.ReadLine());
+    return numberQuarter;
+}
+
+
+
+int quarter = GetInfo("Введите номер четверти: ");
+Console.WriteLine($"Диапазон координат в четверти {CoordinateRange(numberQuarter)}");
+
---
+++ C#/seminar/third/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+double Lenght(int xA, int yA, int xB, int yB)
+{
+    double distance = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
+    return distance;
+}
+
+
+int GetInfo( string message)
+{
+    Console.Write(message);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+
+int CoordinateA1 = GetInfo("Введите координатy x точки A:  ");
+int CoordinateA2 = GetInfo("Введите координатy y точки A:  ");
+int CoordinateB1 = GetInfo("Введите координатy x точки A:  ");
+int CoordinateB2 = GetInfo("Введите координатy y точки A:  ");
+
+
+Console.WriteLine($"Расстояние между двумя точками на плоскости равно {Lenght(CoordinateA1, CoordinateA2, CoordinateB1, CoordinateB2)}");
