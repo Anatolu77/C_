55ad0bce7f9c39db87d85da24f46ffacc52b0903
anatoly <czuczkov1997@mail.ru>
2023-07-17 22:37:16 +0300

    add new task

--- C#/seminar/third/task3/Program.cs
+++ C#/seminar/third/task3/Program.cs
@@ -4,12 +4,23 @@
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
 
-int DefSqr( int number)
+void DefSqr( int number)
 {
-    for ()
+    Console.Write($"{number} -> ");
+    for (int i = 1; i < number; i++)
+    {
+        Console.Write(i * i + ",");
+    }
+    Console.WriteLine(number * number + ".");
+}
+
+int GetInfo( string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
 }
 
 
-
 int number = GetInfo("Введите число: ");
-Console.WriteLine($"");
+DefSqr(number);
